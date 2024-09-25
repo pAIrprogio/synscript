a1c6f92a55b5604c1d7eb6170ea546127cc5416e
Yann Leflour <yleflour@gmail.com>
2024-09-26 00:36:28 +0200

    add text to synstack

--- packages/synscript/src/syn-script.index.ts
+++ packages/synscript/src/syn-script.index.ts
@@ -2,6 +2,12 @@ export { JsonParseException, json } from "@synstack/json";
 export { pipe, type Pipe } from "@synstack/pipe";
 export { resolvable, type Resolvable, type Resolver } from "@synstack/resolved";
 export { str, type Str } from "@synstack/str";
+export {
+  TextParseExtraItemException,
+  t,
+  tParse,
+  type Text,
+} from "@synstack/text";
 export { ArticleNotFoundException, web } from "@synstack/web";
 export { xml, type Xml } from "@synstack/xml";
 export { yaml } from "@synstack/yaml";
