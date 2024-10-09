6b341a69da1442a1da1a2277f195002fabbb6943
Yann Leflour <yleflour@gmail.com>
2024-10-09 14:53:23 +0200

    update synscript imports

--- packages/synscript/src/plop.ts
+++
@@ -1,3 +0,0 @@
-import { str } from "@synstack/str";
-
-const plop = str("Hello World").str;
--- packages/synscript/src/syn-script.index.ts
+++ packages/synscript/src/syn-script.index.ts
@@ -1,4 +1,16 @@
+export { enhance, enhanceFactory, type Enhanced } from "@synstack/enhance";
+export { FsDir, FsFile, dir, file } from "@synstack/fs";
+export { git } from "@synstack/git";
+export { glob } from "@synstack/glob";
 export { JsonParseException, json } from "@synstack/json";
+export { MdDoc, md } from "@synstack/markdown";
+export {
+  PathNotInCwdException,
+  path,
+  type AbsolutePath,
+  type AnyPath,
+  type RelativePath,
+} from "@synstack/path";
 export { pipe, type Pipe } from "@synstack/pipe";
 export { resolvable, type Resolvable, type Resolver } from "@synstack/resolved";
 export { str, type Str } from "@synstack/str";
