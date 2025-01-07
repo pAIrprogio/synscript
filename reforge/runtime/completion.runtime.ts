a41238ef36a92ba55839ae4b4219bdd2677224e2
Yann Leflour <yleflour@gmail.com>
2025-01-07 10:29:28 +0100

    chore(reforge-local): update to synstack v2

--- reforge/runtime/completion.runtime.ts
+++ reforge/runtime/completion.runtime.ts
@@ -1,10 +1,10 @@
-import { Anthropic } from "@anthropic-ai/sdk";
-import { fsCache, t } from "@synstack/synscript";
-import { AnthropicRunner, runner } from "@synstack/synscript/anthropic";
+import { createAnthropic } from "@ai-sdk/anthropic";
+import { fsCache } from "@synstack/synscript";
 import { completion } from "@synstack/synscript/llm";
+import { includeAssistantMessage } from "@synstack/synscript/llm/middleware";
 import { reforgeDir } from "./workspace.runtime.ts";
 
-const anthropicClient = new Anthropic({
+const anthropic = createAnthropic({
   apiKey: process.env.ANTHROPIC_API_KEY,
 });
 
@@ -12,13 +12,10 @@ export const cacheDir = reforgeDir.to(".cache");
 
 export const baseCache = fsCache(cacheDir);
 
-export const completionRunner = runner
-  .client(anthropicClient)
-  .model(AnthropicRunner.MODELS.CLAUDE_3_5_MEDIUM)
-  .continues(3)
-  .retries(2);
-
-export const baseCompletion = completion.maxTokens(4000).temperature(0.5)
-  .system(t`
-    You are an expert software developer.
-  `);
+export const baseCompletion = completion
+  .maxTokens(8000)
+  .temperature(0.5)
+  .middlewares([includeAssistantMessage])
+  .model(anthropic("claude-3-5-sonnet-latest"))
+  .maxSteps(3)
+  .maxRetries(2);
