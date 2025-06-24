c3942d5d56411186e3ce9336c8f57f248dcb0395
Yann Leflour <yann.leflour.ext@qonto.com>
2025-06-24 22:44:44 +0200

    chore: remove reforge

--- reforge/runtime/workspace.runtime.ts
+++
@@ -1,8 +0,0 @@
-import { dir, type FsFile } from "@synstack/fs";
-
-export const rootDir = dir(import.meta.dirname).to("../../");
-export const reforgeDir = rootDir.to("reforge");
-
-export const autoDir = (file: FsFile) => {
-  if (file.isInDir(reforgeDir)) return reforgeDir;
-};
